b80d119ff8ecb2f1c037bcc877e355e81df9f3be
itzkaia <105882737+itzkaia@users.noreply.github.com>
2023-12-03 01:07:19 -0800

    Add files via upload

--- Lab 4 - Arduino/7segCountdown/7segCountdown.ino
+++ Lab 4 - Arduino/7segCountdown/7segCountdown.ino
@@ -1,19 +1,116 @@
-#include <SegmentDisplay.h>
-SegmentDisplay segmentDisplay(2, 3, 4, 5, 6, 7, 8, 9);
 void setup() {
   // put your setup code here, to run once:
-  
+  for (int i = 6; i <= 9; i++) {
+    pinMode(i, OUTPUT);
+  }
+
+  for (int i = 11; i <= 13; i++) {
+    pinMode(i, OUTPUT);
+  }
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  /*Display only the decimal point*/
-  //segmentDisplay.displayDecimalPoint();
+  Serial.begin(9000);
 
-  //segmentDisplay.displayHex(12, false);
+  // 9
+  digitalWrite(6, LOW); // a
+  digitalWrite(7, LOW); // b
+  digitalWrite(8, LOW); // c
+  digitalWrite(9, HIGH); // d
+  digitalWrite(11, HIGH); // e
+  digitalWrite(12, LOW); // f
+  digitalWrite(13, LOW); // g
+  delay(1500);
 
-  for (int i = 9; i >= 0; i--) {
-    segmentDisplay.displayHex(i, true);
-    delay(1500); // Optional delay to make the countdown more readable
-  }
+  // 8
+  digitalWrite(6, LOW); 
+  digitalWrite(7, LOW); 
+  digitalWrite(8, LOW); 
+  digitalWrite(9, LOW); 
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(13, LOW);
+  delay(1500);
+
+  // 7
+  digitalWrite(6, LOW); 
+  digitalWrite(7, LOW); 
+  digitalWrite(8, LOW); 
+  digitalWrite(9, HIGH); 
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH);
+  delay(1500);
+
+  // 6
+  digitalWrite(6, LOW); 
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW); 
+  digitalWrite(9, LOW); 
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(13, LOW);
+  delay(1500);
+
+  // 5
+  digitalWrite(6, LOW); 
+  digitalWrite(7, HIGH);
+  digitalWrite(8, LOW); 
+  digitalWrite(9, LOW); 
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW);
+  digitalWrite(13, LOW);
+  delay(1500);
+
+  // 4
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW); 
+  digitalWrite(8, LOW); 
+  digitalWrite(9, HIGH); 
+  digitalWrite(11, HIGH);
+  digitalWrite(12, LOW); 
+  digitalWrite(13, LOW); 
+  delay(1500);
+
+  // 3
+  digitalWrite(6, LOW); 
+  digitalWrite(7, LOW); 
+  digitalWrite(8, LOW); 
+  digitalWrite(9, LOW); 
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, LOW);
+  delay(1500);
+
+  // 2
+  digitalWrite(6, LOW); 
+  digitalWrite(7, LOW); 
+  digitalWrite(8, HIGH);
+  digitalWrite(9, LOW); 
+  digitalWrite(11, LOW); 
+  digitalWrite(12, HIGH);
+  digitalWrite(13, LOW);
+  delay(1500);
+
+  // 1
+  digitalWrite(6, HIGH);
+  digitalWrite(7, LOW); 
+  digitalWrite(8, LOW); 
+  digitalWrite(9, HIGH); 
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH);
+  delay(1500);
+
+  // 0
+  digitalWrite(6, LOW); 
+  digitalWrite(7, LOW); 
+  digitalWrite(8, LOW); 
+  digitalWrite(9, LOW); 
+  digitalWrite(11, LOW); 
+  digitalWrite(12, LOW); 
+  digitalWrite(13, HIGH);
+  delay(1500);
 }
